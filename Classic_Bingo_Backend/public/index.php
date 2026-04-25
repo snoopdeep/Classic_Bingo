ed1f085dac178f1efec8cb134b93ab4d5b6bad74
snoopdeep <snoopdeep7@gmail.com>
2026-04-26 03:23:31 +0530

    fix cors

--- Classic_Bingo_Backend/public/index.php
+++ Classic_Bingo_Backend/public/index.php
@@ -1,7 +1,15 @@
 <?php
 
-// allow CORS 
-header("Access-Control-Allow-Origin: *");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? 'http://127.0.0.1:8080';
+header("Access-Control-Allow-Origin: $origin");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-Signature, X-Timestamp");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 
 try {
